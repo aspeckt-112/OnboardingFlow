e048d3448415732c1aa5b14414263ca37e366e25
aspeckt112 <114807473+aspeckt-112@users.noreply.github.com>
2026-04-23 16:39:58 +0100

    project creation

---
+++ OnboardingFlow/ContentView.swift
@@ -0,0 +1,19 @@
+// 23/04/2026 - aspeckt112
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,12 @@
+// 23/04/2026 - aspeckt112
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
